8a6a58c0a6d6e02284b987d1038f996c6443c0d1
SuDelk <sudul.fernando@gmail.com>
2024-04-03 13:32:02 +0530

    intialization

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import PageTitle from './components/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>AudiSense</title>
-        <link rel="icon" href="/audisense.ico" />
-      </Head>
-      <body>
-        <h1>Coming Soon</h1>
-        <h2>Our website is under construction</h2>
-        <h3>Stay tuned for updates</h3>
-      </body>
+      <PageTitle title="AudiSense"/>
       </div>
   );
 }
