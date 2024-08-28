6258d91be6beef7fb67fde29eca6e3b26fe6f46d
Randula Mawaththa <randula98@gmail.com>
2024-08-29 03:56:01 +0530

    feat: Patient Add Modal Implemented
    Signed-off-by: Randula Mawaththa <randula98@gmail.com>

--- src/pages/Patients/Patients.jsx
+++ src/pages/Patients/Patients.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Edit, Trash } from "lucide-react";
+import AddNewPatientModal from "../../components/modals/AddNewPatientModal";
 
 function Patients() {
+
+  const [openAddNewPatientModal, setOpenAddNewPatientModal] = useState(false);
+
   const patientsJson = [
     {
       Name: "Emily Johnson",
@@ -71,71 +75,88 @@ function Patients() {
   };
 
   return (
-      <div className="p-4 px-10">
-        <h1 className="text-4xl font-nunito font-bold mb-8">Patients</h1>
-        <div className="min-w-full overflow-hidden overflow-x-scroll">
-          <table className="w-full table-auto">
-            <thead>
-              <tr className=" text-purple-600 font-nunito">
-                <th className="px-4 py-2">Name</th>
-                <th className="px-4 py-2">Age</th>
-                <th className="px-4 py-2">Gender</th>
-                <th className="px-4 py-2">Blood Pressure</th>
-                <th className="px-4 py-2">Heart Rate</th>
-                <th className="px-4 py-2">Temperature</th>
-                <th className="px-4 py-2">Weight</th>
-                <th className="px-4 py-2">Height</th>
-                <th className="px-4 py-2">Diagnosis</th>
-                <th className="px-4 py-2">Edit</th>
-                <th className="px-4 py-2">Delete</th>
-                {/* New column for actions */}
-              </tr>
-            </thead>
-            <tbody>
-              {patientsJson.map((patient, index) => (
-                <tr
-                  key={index + 1}
-                  className={
-                    index % 2 === 0
-                      ? "bg-purple-200 font-nunito"
-                      : "bg-purple-300 font-nunito"
-                  }
-                >
-                  <td className="border px-4 py-2">{patient.Name}</td>
-                  <td className="border px-4 py-2">{patient.Age}</td>
-                  <td className="border px-4 py-2">{patient.Gender}</td>
-                  <td className="border px-4 py-2">{patient.BloodPressure}</td>
-                  <td className="border px-4 py-2">{patient.HeartRate}</td>
-                  <td className="border px-4 py-2">{patient.Temperature}</td>
-                  <td className="border px-4 py-2">{patient.Weight}</td>
-                  <td className="border px-4 py-2">{patient.Height}</td>
-                  <td className="border px-4 py-2">{patient.Diagnosis}</td>
-                  <td className="border px-4 py-2">
-                    <div className="flex justify-center">
-                      <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
-                        onClick={() => handleEdit(patient)}
-                      >
-                        <Edit size={20} />
-                      </button>
-                    </div>
-                  </td>
-                  <td className="border px-4 py-2">
-                    <div className="flex justify-center">
-                      <button
-                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                        onClick={() => handleDelete(patient)}
-                      >
-                        <Trash size={20} />
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+    <div className="p-4 px-10">
+      <div className="flex justify-between items-center mb-8">
+        <h1 className="text-4xl font-nunito font-bold">Patients</h1>
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+
+          onClick={() => {
+            setOpenAddNewPatientModal(true);
+          }}
+        >
+          Add New Patient
+        </button>
       </div>
+      <div className="min-w-full overflow-hidden overflow-x-scroll">
+        <table className="w-full table-auto">
+          <thead>
+            <tr className=" text-purple-600 font-nunito">
+              <th className="px-4 py-2">Name</th>
+              <th className="px-4 py-2">Age</th>
+              <th className="px-4 py-2">Gender</th>
+              <th className="px-4 py-2">Blood Pressure</th>
+              <th className="px-4 py-2">Heart Rate</th>
+              <th className="px-4 py-2">Temperature</th>
+              <th className="px-4 py-2">Weight</th>
+              <th className="px-4 py-2">Height</th>
+              <th className="px-4 py-2">Diagnosis</th>
+              <th className="px-4 py-2">Edit</th>
+              <th className="px-4 py-2">Delete</th>
+              {/* New column for actions */}
+            </tr>
+          </thead>
+          <tbody>
+            {patientsJson.map((patient, index) => (
+              <tr
+                key={index + 1}
+                className={
+                  index % 2 === 0
+                    ? "bg-purple-200 font-nunito"
+                    : "bg-purple-300 font-nunito"
+                }
+              >
+                <td className="border px-4 py-2">{patient.Name}</td>
+                <td className="border px-4 py-2">{patient.Age}</td>
+                <td className="border px-4 py-2">{patient.Gender}</td>
+                <td className="border px-4 py-2">{patient.BloodPressure}</td>
+                <td className="border px-4 py-2">{patient.HeartRate}</td>
+                <td className="border px-4 py-2">{patient.Temperature}</td>
+                <td className="border px-4 py-2">{patient.Weight}</td>
+                <td className="border px-4 py-2">{patient.Height}</td>
+                <td className="border px-4 py-2">{patient.Diagnosis}</td>
+                <td className="border px-4 py-2">
+                  <div className="flex justify-center">
+                    <button
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+                      onClick={() => handleEdit(patient)}
+                    >
+                      <Edit size={20} />
+                    </button>
+                  </div>
+                </td>
+                <td className="border px-4 py-2">
+                  <div className="flex justify-center">
+                    <button
+                      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                      onClick={() => handleDelete(patient)}
+                    >
+                      <Trash size={20} />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <AddNewPatientModal
+        visible={openAddNewPatientModal}
+        onClose={() => {
+          setOpenAddNewPatientModal(false);
+        }} />
+    </div>
   );
 }
 
