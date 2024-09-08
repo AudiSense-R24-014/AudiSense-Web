17595a975ad37128e7192a3489a23f3ac16916c2
Randula Mawaththa <randula98@gmail.com>
2024-09-09 00:07:12 +0530

    feat: Identification Tabs Implemented
    Signed-off-by: Randula Mawaththa <randula98@gmail.com>

---
+++ src/pages/Tasks/TaskComponents/IdentificationSubParts/index.jsx
@@ -0,0 +1,7 @@
+import IdentificationTaskGenerate from './IdentificationTaskGenerate';
+import IdentificationViewAll from './IdentificationViewAll';
+
+export {
+    IdentificationTaskGenerate,
+    IdentificationViewAll,
+};
