61764dbd0ac63f39686b77a8e3457fc8b9b34636
SuDelk <sudul.fernando@gmail.com>
2024-09-24 02:00:50 +0530

    orgnization part done

--- src/services/Organization.service.jsx
+++ src/services/Organization.service.jsx
@@ -1,87 +1,115 @@
-import * as URL from './const/url';
-const token = localStorage.getItem('audi-token');
+import * as URL from "./const/url";
+const token = localStorage.getItem("audi-token");
 
 const createOrganization = async (organization) => {
-    const response = await fetch(URL.ORGANIZATIONS, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify(organization),
-    });
-    const data = await response.json();
-    return data;
+  const response = await fetch(URL.ORGANIZATIONS, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(organization),
+  });
+  const data = await response.json();
+  return data;
 };
 
 const getOrganizationByJoinCode = async (code) => {
-    const response = await fetch(URL.ORGANIZATION_BY_JOIN_CODE(code), {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-    });
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(URL.ORGANIZATION_BY_JOIN_CODE(code), {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  return data;
+};
 
 const getOrganizationById = async (id) => {
-    const response = await fetch(URL.ORGANIZATION_BY_ID(id), {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-    });
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(URL.ORGANIZATION_BY_ID(id), {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  return data;
+};
 
 const updateOrganization = async (id, organization) => {
-    const response = await fetch(URL.ORGANIZATION_BY_ID(id), {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify(organization),
-    });
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(URL.ORGANIZATION_BY_ID(id), {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(organization),
+  });
+  const data = await response.json();
+  return data;
+};
 
 const removeTherapist = async (organizationId, therapistId) => {
-    const response = await fetch(URL.ORGANIZATION_REMOVE_THERAPIST, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({ organizationId, therapistId }),
-    });
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(URL.ORGANIZATION_REMOVE_THERAPIST, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ organizationId, therapistId }),
+  });
+  const data = await response.json();
+  return data;
+};
 
-const  makeTherapistAdmin = async (organizationId, therapistId) => {
-    const response = await fetch(URL.ORGANIZATION_MAKE_ADMIN, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({ organizationId, therapistId }),
-    });
-    const data = await response.json();
-    return data;
-}
+const makeTherapistAdmin = async (organizationId, therapistId) => {
+  const response = await fetch(URL.ORGANIZATION_MAKE_ADMIN, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ organizationId, therapistId }),
+  });
+  const data = await response.json();
+  return data;
+};
+
+const isTherapistAdmin = async (organizationId, therapistId) => {
+  const response = await fetch(URL.ORGANIZATION_IS_ADMIN, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ organizationId, therapistId }),
+  });
+  const data = await response.json();
+  return data;
+};
+
+const removeAdminPrivileges = async (organizationId, therapistId) => {
+  const response = await fetch(URL.ORGANIZATION_REMOVE_ADMIN, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ organizationId, therapistId }),
+  });
+  const data = await response.json();
+  return data;
+};
 
 export default {
-    createOrganization,
-    getOrganizationById,
-    updateOrganization,
-    removeTherapist,
-    makeTherapistAdmin,
-    getOrganizationByJoinCode,
+  createOrganization,
+  getOrganizationById,
+  updateOrganization,
+  removeTherapist,
+  makeTherapistAdmin,
+  getOrganizationByJoinCode,
+  isTherapistAdmin,
+  removeAdminPrivileges,
 };
