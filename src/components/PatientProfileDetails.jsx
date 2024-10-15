5a95eb19d14acc18fbeda15bae07fb8e30d589ab
SuDelk <sudul.fernando@gmail.com>
2024-10-15 17:12:52 +0530

    done

--- src/components/PatientProfileDetails.jsx
+++ src/components/PatientProfileDetails.jsx
@@ -6,7 +6,7 @@ const PatientProfileDetails = ({ patient }) => {
 
     return (
         <div className="font-montserrat space-y-4">
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 text-sm">
+            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 text-sm">
                 {/* Onset History */}
                 <div>
                     <h3 className="font-bold text-lg">Onset History</h3>
